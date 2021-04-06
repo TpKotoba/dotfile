815d7c0839b7a88fb211450d6a347f5844bca37b
TpKotoba <tpkotoba@gmail.com>
2021-04-06 21:20:04 +0800

    setting up typora

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,3 +1,12 @@
 # Dotfiles
 ln -s ~/.dotfile/tpkotoba.vimrc ~/.vimrc
 ln -s ~/.dotfile/tpkotoba.gitconfig ~/.gitconfig
+
+# ================================TYPORA================================
+TyporaVersion=`snap list | grep typora | awk '{print $3}'`
+TyporaPath=~/snap/typora/${TyporaVersion}/.config/Typora
+
+ln -sf ~/.dotfile/tpkotoba.typoraprof ${TyporaPath}/profile.data
+ln -sf ~/.dotfile/tpkotoba.typoraconf ${TyporaPath}/conf/conf.default.json
+
+mv ./typora-monospace-theme/monospace.css ./typora-monospace-theme/monospace-dark.css ./typora-monospace-theme/monospace ${TyporaPath}/themes
